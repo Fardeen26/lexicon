be397bd119ceb998f1cea07b9e5bc026f943d73f
Fardeen Mansuri <fardeenmansuri0316@gmail.com>
2024-08-31 22:56:52 +0530

    book view page added

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -3,12 +3,11 @@ import BookList from "./components/BookList";
 
 
 export default async function Home() {
-  const response = await fetch(`https://icanhazdadjoke.com/`);
-  // if(!response.ok) {
-  //   throw new Error('An error occurred while fetching the books')
-  // }
+  const response = await fetch(`${process.env.BACKEND_URL}/books`);
+  if(!response.ok) {
+    throw new Error('An error occurred while fetching the books')
+  }
   const books = await response.json();
-
 
   return (
     <div>
