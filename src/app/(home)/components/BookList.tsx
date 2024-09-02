6e7a905b65f2695a8f20e1ff55416fb7b3a10598
Fardeen Mansuri <fardeenmansuri0316@gmail.com>
2024-09-02 18:43:46 +0530

    added suspence

--- src/app/(home)/components/BookList.tsx
+++ src/app/(home)/components/BookList.tsx
@@ -2,7 +2,13 @@ import React from 'react';
 import BookCard from './BookCard';
 import { Book } from '@/types';
 
-const BookList = async ({books}: {books: Book[]}) => {
+const BookList = async () => {
+
+    const response = await fetch(`${process.env.BACKEND_URL}/books`);
+  if(!response.ok) {
+    throw new Error('An error occurred while fetching the books')
+  }
+  const books = await response.json();
 
     return (
         <div className="grid grid-cols-1 gap-8 md:grid-cols-3 max-w-7xl mx-auto mb-10">
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,18 +1,17 @@
 import Banner from "@/app/(home)/components/Banner";
 import BookList from "./components/BookList";
+import { Suspense } from "react";
+import Loading from "@/components/Loading";
 
 
 export default async function Home() {
-  const response = await fetch(`${process.env.BACKEND_URL}/books`);
-  if(!response.ok) {
-    throw new Error('An error occurred while fetching the books')
-  }
-  const books = await response.json();
-
   return (
     <div>
       <Banner />
-      <BookList books={books}/>
-    </div>
+      <Suspense fallback={<Loading />}>
+
+      </Suspense>
+      <BookList />      
+      </div>
   );
 }
