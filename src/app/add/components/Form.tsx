06e127330bd65952f55ebc5e72cedf66108359fc
Fardeen Mansuri <fardeenmansuri0316@gmail.com>
2024-09-11 15:58:08 +0530

    sonner notifications added

--- src/app/add/components/Form.tsx
+++ src/app/add/components/Form.tsx
@@ -1,5 +1,6 @@
 "use client"
 import React, { useState } from 'react'
+import { Toaster, toast } from 'sonner'
 
 const Form = () => {
   const [bookData, setBookData] = useState<Object>({
@@ -9,7 +10,6 @@ const Form = () => {
     bookPdfUrl: '',
     author: '',
   })
-
   const handleOnChange = async (e:any) => {
         setBookData({ ...bookData, [e.target.name]: e.target.value });
     }
@@ -28,23 +28,24 @@ const Form = () => {
 
         const data = await response.json();
         if(data.success) {
-          console.log("Book Added Successfully")
+          toast.success("Book Added Successfully")
         }else {
-          console.log("Failed To save Book");
+          toast.error(data.msg)
         }
-      } catch (error) {
-        console.log(error)
+      } catch (error: any) {
+        toast.error(error.message)
       }    
 } 
 
   return (
     <div className='h-[80vh] flex justify-center items-center max-sm:px-2'>
+      <Toaster position='bottom-right' richColors/>
         <form className='flex flex-col gap-5 w-[40vw] max-sm:w-full' onSubmit={submitBook}>
-            <input type="text" placeholder='Book Name' name='title' onChange={handleOnChange} className='px-5 py-2 w-full border border-black placeholder:text-black focus:placeholder:text-gray-400 rounded text-black'/>
-            <input type="text" placeholder='Description' name='description' onChange={handleOnChange} className='px-5 py-2 w-full border border-black placeholder:text-black focus:placeholder:text-gray-400 rounded text-black'/>
-            <input type="text" placeholder='Book Image Url' name='bookImage' onChange={handleOnChange} className='px-5 py-2 w-full border border-black placeholder:text-black focus:placeholder:text-gray-400 rounded text-black'/>
-            <input type="text" placeholder='Book PDF Url' name='bookPdfUrl' onChange={handleOnChange} className='px-5 py-2 w-full border border-black placeholder:text-black focus:placeholder:text-gray-400 rounded text-black'/>
-            <input type="text" placeholder='Author' name='author' onChange={handleOnChange} className='px-5 py-2 w-full border border-black placeholder:text-black focus:placeholder:text-gray-400 rounded text-black'/>
+            <input type="text" placeholder='Book Name' name='title' onChange={handleOnChange} className='px-5 py-2 w-full border border-black placeholder:text-black focus:placeholder:text-gray-400 rounded text-black' required/>
+            <input type="text" placeholder='Description' name='description' onChange={handleOnChange} className='px-5 py-2 w-full border border-black placeholder:text-black focus:placeholder:text-gray-400 rounded text-black' required/>
+            <input type="text" placeholder='Book Image Url' name='bookImage' onChange={handleOnChange} className='px-5 py-2 w-full border border-black placeholder:text-black focus:placeholder:text-gray-400 rounded text-black' required/>
+            <input type="text" placeholder='Book PDF Url' name='bookPdfUrl' onChange={handleOnChange} className='px-5 py-2 w-full border border-black placeholder:text-black focus:placeholder:text-gray-400 rounded text-black' required/>
+            <input type="text" placeholder='Author' name='author' onChange={handleOnChange} className='px-5 py-2 w-full border border-black placeholder:text-black focus:placeholder:text-gray-400 rounded text-black' required/>
             <button className='h-10 rounded-md bg-primary-500 px-4 py-2 text-sm font-medium text-white transition-all hover:bg-primary-600 active:bg-primary-700 mt-5'>Add Book</button>
         </form>
     </div>
