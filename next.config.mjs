918652a3c5508dbc632a36bd8035446478d5f84f
Fardeen Mansuri <fardeenmansuri0316@gmail.com>
2024-09-15 19:37:41 +0530

    image hostname added

--- next.config.mjs
+++ next.config.mjs
@@ -13,6 +13,10 @@ const nextConfig = {
             {
                 protocol: 'https',
                 hostname: 'm.media-amazon.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'x.com',
             }
         ],
     },
