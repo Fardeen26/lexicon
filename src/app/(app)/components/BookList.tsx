53eb58633f0428a497484b966d17aeddcba76aca
Fardeen Mansuri <fardeenmansuri0316@gmail.com>
2024-10-27 13:13:15 +0530

    fix: converted booklist into server component

--- src/app/(app)/components/BookList.tsx
+++ src/app/(app)/components/BookList.tsx
@@ -1,44 +1,21 @@
-'use client'
-
 import BookCard from './BookCard';
 import { Book } from '@/types';
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-import { toast, Toaster } from 'sonner';
 
-const BookList = () => {
-    const [books, setBooks] = useState<Book[]>([])
-    const [isFetching, setIsFetching] = useState<boolean>(false)
+const BookList = async () => {
+    const apiUrl = `${process.env.FRONTEND_URL || 'https://lexicon-sand.vercel.app'}/api/books`;
 
-    const fetchBooks = async () => {
-        setIsFetching(true)
-        try {
-            const isServer = typeof window === 'undefined';
+    const response = await fetch(apiUrl, { cache: 'no-store' });
+    const data = await response.json();
 
-            const apiUrl = isServer
-                ? `${process.env.FRONTEND_URL || 'https://lexicon-sand.vercel.app'}/api/books`
-                : '/api/books';
-
-            const response = await axios.get(apiUrl);
-
-            setBooks(response.data.message)
-        } catch (error) {
-            toast.error(`Error while fetching books ${error}`)
-        } finally {
-            setIsFetching(false)
-        }
+    if (!response.ok) {
+        throw new Error(`Error while fetching books: ${data.error}`);
     }
 
-    useEffect(() => {
-        fetchBooks()
-    }, [])
+    const books: Book[] = data.message;
 
     return (
         <div className="grid min-h-screen grid-cols-1 gap-8 md:grid-cols-3 max-w-7xl mx-auto mb-10 max-sm:px-4 max-[1285px]:px-7">
-            <Toaster position="bottom-right" richColors />
-            {
-                isFetching && <p>Fetching...</p>
-            }
+            {books.length === 0 && <p>No books found.</p>}
             {books.map((book: Book) => (
                 <BookCard key={book._id} book={book} />
             ))}
