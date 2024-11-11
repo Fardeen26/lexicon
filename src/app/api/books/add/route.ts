ac50404c8a3e594fbcc70a8819be4bf1ad52722c
Fardeen Mansuri <fardeenmansuri0316@gmail.com>
2024-11-11 22:03:37 +0530

    fix: protected the api routes

--- src/app/api/books/add/route.ts
+++ src/app/api/books/add/route.ts
@@ -2,8 +2,9 @@ import dbConnect from "@/lib/dbConnect";
 import Book from "@/models/Book";
 import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
+import { getServerSession } from "next-auth/next";
 
-// Define Zod schema for book data validation
+
 const bookSchema = z.object({
   title: z
     .string({ required_error: "Title is required!" })
@@ -28,40 +29,44 @@ const bookSchema = z.object({
 });
 
 export async function POST(req: NextRequest) {
-    await dbConnect();
+  const session = await getServerSession();
+  if (!session) {
+    return NextResponse.json({ success: false, message: "You are Unauthorized" }, { status: 401 });
+  }
+  await dbConnect();
 
-    try {
-        const body = await req.json();
+  try {
+    const body = await req.json();
 
-        const parsedData = bookSchema.parse(body);
+    const parsedData = bookSchema.parse(body);
 
-        const book = new Book({
-            title: parsedData.title,
-            description: parsedData.description,
-            coverImage: parsedData.bookImage,
-            file: parsedData.bookPdfUrl,
-            author: parsedData.author,
-        });
+    const book = new Book({
+      title: parsedData.title,
+      description: parsedData.description,
+      coverImage: parsedData.bookImage,
+      file: parsedData.bookPdfUrl,
+      author: parsedData.author,
+    });
 
-        await book.save();
+    await book.save();
 
-        return NextResponse.json(
-            { message: 'Book Added Successfully!', success: true },
-            { status: 200 }
-        );
+    return NextResponse.json(
+      { message: 'Book Added Successfully!', success: true },
+      { status: 200 }
+    );
 
 
-    } catch (error) {
-        if (error instanceof z.ZodError) {
-            return NextResponse.json(
-                { message: "Validation Error", errors: error.errors, success: false },
-                { status: 400 }
-            );
-        }
-        
-        return NextResponse.json(
-            { message: `Error While Adding Book: ${error}`, success: false },
-            { status: 500 }
-        );
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return NextResponse.json(
+        { message: "Validation Error", errors: error.errors, success: false },
+        { status: 400 }
+      );
     }
+
+    return NextResponse.json(
+      { message: `Error While Adding Book: ${error}`, success: false },
+      { status: 500 }
+    );
+  }
 }
