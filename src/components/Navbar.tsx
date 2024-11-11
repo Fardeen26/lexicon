f6bfdf5269cc4a6c32ba28e7d9e51b78bb1e4eb8
Fardeen Mansuri <fardeenmansuri0316@gmail.com>
2024-11-11 21:53:54 +0530

    feat: implemented the google auth provider

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,23 @@
+"use client"
+
 import React from 'react'
 import Link from 'next/link';
 import HexagonIcon from '@/utils/HexagonIcon';
 import BookIcon from '@/utils/BookIcon';
+import { signIn, useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
 
 const Navbar = () => {
+    const session = useSession();
+    const router = useRouter()
+    const handleAuthLogic = () => {
+        if (session.status === 'authenticated') {
+            router.push('/add')
+        }
+        else {
+            signIn()
+        }
+    }
     return (
         <nav className="border-b max-sm:px-4 max-[1285px]:px-10">
             <div className="max-w-7xl mx-auto flex items-center justify-between py-4">
@@ -21,11 +35,9 @@ const Navbar = () => {
                     </Link>
                 </div>
                 <div className="flex items-center gap-4">
-                    <Link href={'/add'}>
-                    <button className="h-10 rounded-md bg-primary-500 px-4 py-2 text-sm font-medium text-white transition-all hover:bg-primary-600 active:bg-primary-700 border hover:bg-transparent hover:border-black hover:text-black">
+                    <button className="h-10 rounded-md bg-primary-500 px-4 py-2 text-sm font-medium text-white transition-all hover:bg-primary-600 active:bg-primary-700 border hover:bg-transparent hover:border-black hover:text-black" onClick={handleAuthLogic}>
                         Add Your Book
                     </button>
-                    </Link>
                 </div>
             </div>
         </nav>
