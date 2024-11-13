b4c9348ef0a9c374b85a2a9a3a224af4a7c0d27f
Fardeen Mansuri <fardeenmansuri0316@gmail.com>
2024-11-13 19:14:15 +0530

    fix: minor changes

--- src/app/signin/components/SignIn.tsx
+++ src/app/signin/components/SignIn.tsx
@@ -9,7 +9,7 @@ const SignIn = () => {
     }
     return (
         <div className="h-52 flex flex-col items-center">
-            <h1 className="text-center font-semibold text-3xl">Welcome to Lexicon</h1>
+            <h1 className="text-center font-semibold text-3xl dark:text-white">Welcome to Lexicon</h1>
             <button className="font-bold py-2 px-4 rounded flex items-center bg-gray-200 space-x-4 mt-5" onClick={handleClick}>
                 <span><FcGoogle className="w-5 h-5" /></span> <span>Continue with Google</span>
             </button>
