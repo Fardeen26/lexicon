f768deb079b370382a889b4a9bd2f9d94d301c6e
Fardeen Mansuri <fardeenmansuri0316@gmail.com>
2024-12-02 20:09:12 +0530

    fix: caching problem

--- src/app/api/books/route.ts
+++ src/app/api/books/route.ts
@@ -2,20 +2,40 @@ import { Book as BookI } from "@/types";
 import Book from "@/models/Book";
 import dbConnect from "@/lib/dbConnect";
 
+export const runtime = "edge"
 
 export async function GET(req: Request) {
     await dbConnect();
     try {
         const books: BookI[] = await Book.find({});
-        return Response.json(
-            { message: books, success: true },
-            { status: 201 }
+        return new Response(
+            JSON.stringify({
+                message: books,
+                success: true
+            }),
+            {
+                status: 200,
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Cache-Control': 'no-store',
+                },
+            }
         );
+
     } catch (error) {
         console.error("Error while fetching books:", error);
-        return Response.json(
-            { message: 'Error while fetching books', success: false },
-            { status: 501 }
+        return new Response(
+            JSON.stringify({
+                message: 'Error while fetching books',
+                success: false
+            }),
+            {
+                status: 501,
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Cache-Control': 'no-store',
+                },
+            }
         );
     }
 }
